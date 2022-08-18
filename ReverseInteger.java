13921c05f402298ef3fdab9feec9fc5b09e83946
iangowland22 <110187447+iangowland22@users.noreply.github.com>
2022-08-18 16:09:12 -0500

    Add files via upload

---
+++ ReverseInteger.java
@@ -0,0 +1,70 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes 
+the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+Input: x = 123
+Output: 321
+
+Example 2:
+Input: x = -123
+Output: -321
+
+Example 3:
+Input: x = 120
+Output: 21
+*/
+
+public class ReverseInteger {
+
+    public static int reverseInteger(Integer toReverse){
+
+        String str = toReverse.toString();
+        int index = str.length()-1;
+        String reversed = "";
+        while(index >= 0){
+            if(index == 0){
+                if(str.charAt(index) == '-'){
+                    reversed = "-" + reversed;
+                    break;
+                }
+                else if(str.charAt(index) == 0){
+                    index--;
+                    break;
+                } 
+            } 
+            reversed += str.charAt(index);
+            index--;
+        }
+
+        int converted = 0;
+        try {
+            converted = Integer.parseInt(reversed);
+        } catch (Exception e) {
+            System.err.println("Reversed number out of bounds. " + e.getMessage());
+            return 0;
+        }
+        return converted;
+    }
+
+    public static void main(String[] args){
+
+        int test = 3234698;
+        System.out.println("Original number: " + test);
+        System.out.println("Reversed: " + reverseInteger(test) + "\n");
+
+        test = Integer.MAX_VALUE;
+        System.out.println("Original number: " + test);
+        System.out.println("Reversed: " + reverseInteger(test) + "\n");
+
+        test = -34567;
+        System.out.println("Original number: " + test);
+        System.out.println("Reversed: " + reverseInteger(test) + "\n");
+
+        test = 120;
+        System.out.println("Original number: " + test);
+        System.out.println("Reversed: " + reverseInteger(test) + "\n");
+    }
+    
+}
